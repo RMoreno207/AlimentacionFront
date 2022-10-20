9d3a9bd730e48cb7774ef69b84a9c09e0fb28572
pablomoga9 <moyagarcia99@gmail.com>
2022-10-20 16:34:42 +0200

    test push

--- src/components/common/Discounts/MiniCard/MiniCard.jsx
+++ src/components/common/Discounts/MiniCard/MiniCard.jsx
@@ -4,8 +4,10 @@ const MiniCard = (props) => {
   const info = props.value;
 
   return (
+    
     <div className="miniCardContainer">
       <div className="miniText">
+        {console.log("main push")}
         <h4>{info.place_name}</h4>
       </div>
       <img style={{ width: "100px" }} src={info.thumbnail} />
